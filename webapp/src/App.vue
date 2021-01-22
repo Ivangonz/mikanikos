6a243766d77dcec7cbe156c190c1eae3d2822161
Ivan Gonzalez Ibarra <ivan.gonzalez@liveviewtech.com>
2021-01-21 22:40:07 -0700

    Added the frontend directory.

---
+++ webapp/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- client side greeting -->
+    <p>{{greeting}}</p>
+    <!-- server side greeting (api) -->
+    <p>{{flaskGreeting}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            greeting: 'Hello, Vue!',
+            flaskGreeting: ''
+        }
+    },
+    created: async function(){
+        const gResponse = await fetch("http://localhost:5000/greeting");
+        const gObject = await gResponse.json();
+        this.flaskGreeting = gObject.greeting;
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
